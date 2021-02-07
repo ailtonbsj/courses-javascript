536025d1029814ccbff134ad5adc9b55fbeff4d7
ailtonbsj <ailton.ifce@gmail.com>
2021-02-06 23:28:24 -0300

    configured webpack with babel

---
+++ tdd-na-pratica-justen/59-configurando-webpak/webpack.config.js
@@ -0,0 +1,23 @@
+const webpack = require('webpack')
+
+module.exports = {
+  entry: './app.js',
+  output: {
+    filename: './build.js'
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
+}
