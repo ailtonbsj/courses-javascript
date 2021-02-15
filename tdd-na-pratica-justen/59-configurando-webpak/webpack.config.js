89cf0ffeeba0349b7d395bdcdb2ceb83c8776d37
ailtonbsj <ailton.ifce@gmail.com>
2021-02-15 14:09:25 -0300

    added teaser

--- tdd-na-pratica-justen/59-configurando-webpak/webpack.config.js
+++ tdd-na-pratica-justen/59-configurando-webpak/webpack.config.js
@@ -1,6 +1,9 @@
 const webpack = require('webpack')
+const TerserPlugin = require("terser-webpack-plugin")
+const path = require('path')
 
 module.exports = {
+  devtool: 'source-map',
   entry: './app.js',
   output: {
     filename: './build.js'
@@ -19,5 +22,16 @@ module.exports = {
         }
       }
     ]
-  }
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin({
+      parallel: true,
+    })],
+  },
+  devServer: {
+    contentBase: '.',
+    compress: true,
+    port: 9000,
+  },
 }
