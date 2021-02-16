aa3170eb8f9b115b1728e9cc621b39438a67007a
ailtonbsj <ailton.ifce@gmail.com>
2021-02-15 21:45:53 -0300

    using chai

--- tdd-na-pratica-justen/59-configurando-webpak/tests/main.spec.js
+++ tdd-na-pratica-justen/59-configurando-webpak/tests/main.spec.js
@@ -1,27 +1,28 @@
+var expect = require('chai').expect
+
 describe('Main', function () {
+  var arr
 
-  before(function () {
-    console.log('before')
-  })
-  after(function () {
-    console.log('after')
-  })
-  afterEach(function () {
-    console.log('afterEach')
+  beforeEach(function() {
+    arr = [1, 2, 3]
   })
 
-  describe('Method A', function () {
-    context('Case 1', function () {
-      it('shoud happen blabla', function () {
-        console.log('test1')
-      })
-    })
+  it('should be an array', function() {
+    expect(arr).to.be.a('array')
   })
-  describe('Method B', function () {
-    context('Case 1', function () {
-      it('shoud happen sh sh', function () {
-        console.log('test2')
-      })
-    })
+
+  it('should have a size of 4 when push another value to the array', function () {
+    arr.push(4)
+    expect(arr).to.have.lengthOf(4)
+  })
+
+  it('should have a size of 2 when pop a value from the array', function () {
+    arr.pop()
+    expect(arr).to.have.lengthOf(2)
+  })
+
+  it('should remove the value 3 when use pop in the array', function(){
+    arr.pop()
+    expect(arr).to.not.include(3)
   })
 })
